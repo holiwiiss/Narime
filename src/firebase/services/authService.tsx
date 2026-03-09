cc8650d5cd0fcd2458fae9a281d381013534b315
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-03-09 13:42:27 +0100

    feature: login basics

--- src/firebase/services/authService.tsx
+++ src/firebase/services/authService.tsx
@@ -10,6 +10,11 @@ export const register = async (email: string, password: string) => {
   }
 };
 
-export const login = (email: string, password: string) =>
-  signInWithEmailAndPassword(auth, email, password);
-
+export const login = async (email: string, password: string) => {
+  try{
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    return { user: userCredential.user, error: null };
+  } catch(error:any){
+    return { user: null, error };
+  }
+}
--- src/view/login/Login.tsx
+++ src/view/login/Login.tsx
@@ -1,19 +1,71 @@
-import { getAuth } from "firebase/auth";
+import { useState } from "react";
+import { login } from "../../firebase/services/authService";
 import "./login.scss"
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        userEmail:'',
+        userPassword:''
+    })
+
+    const [errors, setErrors] = useState({
+        errorEmail:'',
+        errorPassword: '',
+    })
+
+    const validateForm = async () => {
+        let isCorrect = true
+        const errorsValidation = { errorEmail:'', errorPassword:''}
+
+        if(!formData.userEmail){
+            isCorrect = false
+            errorsValidation.errorEmail = 'Introduce un correo electrónico'
+        }
+
+        if(!formData.userPassword || formData.userPassword.length < 6){
+            isCorrect = false
+            errorsValidation.errorPassword = 'La contraseña no es válida'
+        }
+
+        setErrors(errorsValidation)
+
+        if(isCorrect){
+            validateFirebase()
+        }
+    }
+
+    const validateFirebase = async () => {
+        const { user, error } = await login(formData.userEmail, formData.userPassword);
+
+        if(error){
+            if(error.code ==='auth/invalid-email'){
+                setErrors(prev => ({ ...prev, errorEmail: "Correo electrónico inválido" }));
+            }else if(error.code ==='auth/user-disabled'){
+                setErrors(prev => ({ ...prev, errorEmail: "Usuario deshabilitado" }));
+            }else if(error.code ==='auth/user-not-found'){
+                setErrors(prev => ({ ...prev, errorEmail: "El correo electrónico no existe"}));
+            }else if(error.code ==='auth/wrong-password'){
+                setErrors(prev => ({ ...prev, errorPassword: "La contraseña no coincide"}));
+            }else{
+                setErrors(prev => ({ ...prev, errorPassword: "Ha habido algún error"}));
+            }   
+        }else {
+            console.log("Iniciada sesión en:", user);
+        }
+    }
 
     return(
         <>
             <div className="colunm">
                 <label>Email</label>
-                <input type="email"></input>
-                <span>El correo electronico no es correcto</span>
+                <input type="email" onChange={e => setFormData(prev => ({...prev, userEmail: e.target.value}))}></input>
+                <span>{errors.errorEmail}</span>
 
                 <label>Contraseña</label>
-                <input type="password"></input>
-                <span>La contraseña no es correcta</span>
+                <input type="password" onChange={e => setFormData(prev => ({...prev, userPassword: e.target.value}))}></input>
+                <span>{errors.errorPassword}</span>
 
-                <button>Iniciar sesión</button>
+                <button onClick={validateForm}>Iniciar sesión</button>
             </div>
         </>
     )
