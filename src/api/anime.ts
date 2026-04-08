446bcfdfb7ab4fd3f302709f9ca58c7656791d67
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-04-08 18:04:35 +0200

    directory: getSeasonalAnimes

--- src/api/anime.ts
+++ src/api/anime.ts
@@ -40,3 +40,39 @@ export async function getTopAnime() {
     return [];
   }
 }
+
+
+// para coger los animes de la season
+export async function getSeasonNow() {
+  const request = URL__JIKAN + 'seasons/now?page=1&sfw=true';
+  console.log(request)
+  try {
+    const response = await fetch(request);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    if (!json.data) return [];
+
+    const finalData = json.data.map((anime:any) => ({
+        id: anime.mal_id,
+        title: anime.title,
+        image: anime.images.webp.image_url,
+        score: anime.score,
+        episodes: anime.episodes,
+        //tengo que hacer un bucle aquí también para sacar las cosas
+        genres: anime.genres.name,
+    }));
+    console.log(finalData)
+    return finalData;
+
+  } catch (error:any) {
+    console.error(error.message);
+    return [];
+  }
+}
+
+// para obtener todos los generos
+
