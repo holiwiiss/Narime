39f47596a69b32cc738c87f84ecabe1d37dee669
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-04-16 15:56:33 +0200

    feature: selectors information added to the directory interface

--- src/services/anime-genres/anime-genres.mapper.ts
+++ src/services/anime-genres/anime-genres.mapper.ts
@@ -2,7 +2,30 @@ import type { JikanAnimeGenresType } from "../jikan-API.type";
 import type { AnimeGenresType } from "./anime-genres.type";
 
 export function mapJikanAnimeGenres(data: JikanAnimeGenresType[]): AnimeGenresType[]{
-  return data.map((genre): AnimeGenresType => ({
+  
+  const bannedGenres = [
+    "Hentai",
+    "Erotica",
+    "Ecchi",
+    "Harem",
+    "Reverse Harem",
+    "Magical Sex Shift",
+    "Boys Love",
+    "Girls Love",
+    "Gore",
+    "Adult Cast",
+    "Crossdressing"
+  ]
+
+  const dataMapped = data.map((genre): AnimeGenresType => ({
     name: genre.name,
   }))
+
+  return dataMapped.filter((genre) => {
+    if(bannedGenres.includes(genre.name)){
+      return false
+    }else{
+      return true
+    }
+  })
 }
--- src/view/directory-page/DirectoryPage.tsx
+++ src/view/directory-page/DirectoryPage.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import "./directorypage.scss";
 import type { AnimeListResponse, AnimeListType } from "../../services/anime-list/anime-list.type";
 import { getSeasonalAnimes, getTopAnime, getTrendingAnimes } from "../../services/anime-list/anime-list";
+import type { AnimeGenresType } from "../../services/anime-genres/anime-genres.type";
+import { getAnimeGenres } from "../../services/anime-genres/anime-genres";
 
 const DirectoryPage = () => {
   const [animeList, setAnimeList] = useState<AnimeListType[]>([]);
@@ -13,6 +15,10 @@ const DirectoryPage = () => {
   const [lastPage, setLastPage] = useState<number>(1)
   const navigate = useNavigate()
 
+  const [selected, setSelected] = useState("");
+  const [genresList, setGenresList] = useState<AnimeGenresType[]>([])
+  const yearList = [2026, 2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990]
+  const typeList = ['tv', 'movie', 'ova', 'special', 'ona']
   useEffect(() => {
     const fetchAnimes= async () => {
       try{
@@ -39,6 +45,16 @@ const DirectoryPage = () => {
 
     };
     fetchAnimes();
+
+    const loadGenres = async () => {
+      try{
+        const JSONGenres : AnimeGenresType[] = await getAnimeGenres()
+        setGenresList(JSONGenres);
+      }catch(err){
+        console.log('La api no responde,' + err)
+      }
+    }
+    loadGenres()
   }, [activeCategory, actualPage]);
 
   const activateFilter = (category: "top" | "trending" | "seasonal") => {
@@ -70,6 +86,40 @@ const DirectoryPage = () => {
       <button className={activeCategory === 'trending' ? "bton btn__able" : " bton btn__disable "} onClick={() => activateFilter("trending")}>Trending</button>
       <button className={activeCategory === 'top' ? "bton btn__able" : " bton btn__disable"} onClick={() => activateFilter("top")}>Top 100</button>
       <button className={activeCategory === 'seasonal' ? "bton btn__able" : " bton btn__disable"} onClick={() => activateFilter("seasonal")}>Seasonal</button>
+
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        <option value="">Genre</option>
+
+        {genresList.map((genre) => (
+          <option value={genre.name}>
+            {genre.name}
+          </option>
+        ))}
+
+      </select>
+
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        <option value="">Year</option>
+
+        {yearList.map((year) => (
+          <option value={year}>
+            {year}
+          </option>
+        ))}
+        
+      </select>
+
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        <option value="">Type</option>
+
+        {typeList.map((type) => (
+          <option value={type}>
+            {type}
+          </option>
+        ))}
+        
+      </select>
+
     </div>
 
     <div className="cards__container">
