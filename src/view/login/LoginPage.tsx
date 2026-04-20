e981be178c657e0f62783bd28ebd64e653961e74
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-04-20 14:05:20 +0200

    feature: scss login

--- src/view/login/LoginPage.tsx
+++ src/view/login/LoginPage.tsx
@@ -3,8 +3,7 @@ import{ useForm } from "react-hook-form"
 import type { SubmitHandler } from "react-hook-form"
 import { sileo } from "sileo";
 import type { LoginFormInputs } from "../../types/authTyping";
-
-import "./login.scss";
+import  "./login-page.scss";
 
 const LoginPage = () => {
 
@@ -42,33 +41,74 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className="colunm">
-        <form onSubmit={handleSubmit(onSubmit)} className="colunm">
-          <label>Email</label>
-          <input
-            type="email"
-            {...register('email', { 
-              required: 'Este campo es obligatorio', 
-            })}
-          ></input>
-          {errors.email && <span>{errors.email.message}</span>}
+    <main className="login-page">
+      <section className="login-page__info">
 
-          <label>Contraseña</label>
-          <input
-            type='password'
-            {...register('password', { 
-              required: 'Este campo es obligatorio', 
+        <header className="login-page__header">
+          <img src="#" alt="Narime logo" className="login-page__logo"></img>
+
+          <button className="login-page__back">
+            <img src="#" alt="" />
+            <p>Go back</p>
+          </button>
+        </header>
+
+        <div className="login-page__text">
+          <h1 className="login-page__title"> EVERY STORY MATTERS, YOURS TOO </h1>
+
+          <p className="login-page__description"> Narime allows you to create smart lists, dynamic rankings, and personalized recommendations.</p>
+        </div>
+
+      </section>
+
+      <section className="login-page__form-section">
+        <div className="login-page__form-section-text">
+          <h2 className="login-page__subtitle">Log In</h2>
+          <p className="login-page__subtitle-text"> Sign in to keep tracking the progress of your favorite series.</p>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="login-page__form">
+
+          <div className="login-page__form-field">
+            <label>Email</label>
+            <input
+              type="email"
+              {...register('email', { 
+                required: 'Este campo es obligatorio', 
+              })}
+            ></input>
+            {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          
+          <div className="login-page__form-field">
+            <label>Contraseña</label>
+            <input
+              type='password'
+              {...register('password', { 
+                required: 'Este campo es obligatorio', 
               minLength: {value: 6, message: 'La contraseña debe tener al  menos 6 carácteres'},
               pattern: {value: /^(?=.*[A-Z])(?=.*\d).+$/, message: "Debe tener una mayúscula y un número"}
             })}
-          ></input>
-          {errors.password && <span>{errors.password.message}</span>}
+            ></input>
+            {errors.password && <span>{errors.password.message}</span>}
+          </div>
+          <p className="login-page__form-forget">Forget your password?</p>
 
-          <button type="submit">Iniciar sesión</button>
+          <button className="btn-primary login-page__button" type="submit">Iniciar sesión</button>
+        
         </form>
 
-        <button onClick={loginGoogle}>Inicia sesión con google</button>
-      </div>
+        <div className="login-page__form-separation">
+          <div className="login-page__form-separation-line"></div>
+          <p className="login-page__form-separation-text">O</p>
+          <div className="login-page__form-separation-line"></div>
+        </div>
+
+        <button className="btn-secondary login-page__button" onClick={loginGoogle}>Inicia sesión con google</button>
+        
+        <p className="login-page__form-sing-up">You don't have account? Register here</p>
+      </section>
+    </main>
     </>
   );
 };
