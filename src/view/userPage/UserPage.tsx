440397a15dc5f6a3164897f7787422e032ed48cf
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-05-21 13:43:23 +0200

    feature: userPage scss

--- src/view/userPage/UserPage.tsx
+++ src/view/userPage/UserPage.tsx
@@ -9,6 +9,8 @@ import AnimeCard from "../../components/animeCard/AnimeCard";
 import { fetchMyFavoriteList } from "../../queries/user-page.query";
 import { ModalAddFavorites } from "../../components/modals/ModalAddFavorite/ModalAddFavorite";
 import { ModalRemoveFavorites } from "../../components/modals/ModalRemoveFavorite/ModalRemoveFavorite";
+import LoadingComponent from "../../components/loading/LoadingComponent";
+import ErrorComponent from "../../components/error/ErrorComponent";
 
 const UserPage = () => {
 
@@ -32,48 +34,77 @@ const UserPage = () => {
     enabled: list.length > 0,
   })
 
-  return( <>
-    {user && userData && (
+  return(
+  <>
+    <section className="user-page__content">
+    
+    {isLoadingUser || !userData || !user ? (
+      <LoadingComponent text="Loading user data..."></LoadingComponent>
+    ) : isErrorUser  ? (
+      <ErrorComponent text="An error has ocurred"></ErrorComponent>
+    ) : (
       <>
-        <h1>{userData.username}</h1>
-        <img className="user__image" src={userData.avatar} alt={userData.avatar} />
-        <p>{userData.description}</p>
-        <div>
-          <div>
-            <p>{userData.followersCount}</p>
-            <p>followers</p>
-          </div>
-          <div>
-            <p>{userData.followingCount}</p>
-            <p>following</p>
-          </div>
-          <div>
-            <p>{myList.length ?? 0}</p>
-            <p>ítems in list</p>
-          </div>
-        </div>
-
-        <div>
-          <h2>Tus favoritos</h2>
-          <button className="btn" onClick={() => setIsOpenRemoveFavorite(true)}>Edit favorites</button>
-            <div className="anime-cards__container">
-              {myFavoriteList.map((anime: AnimeCardType) =>(
-                <AnimeCard
-                  key={anime.id}
-                  anime={anime}
-                  variant="minimal"
-                >
-                </AnimeCard>
-              ))}
-              {myFavoriteList.length <= 4 && (
-                <div className="add_favorites" onClick={() => setIsOpenAddFavorite(true)}>
-                  <img className="add__icon" src="#" />
+          <header className="user-page__header">
+            <div className="user-page__header--banner">
+              <button className="btn btn--secondary btn--small">Edit profile</button>
+              <div className="user-page__social--info">
+                <div className="user-page__social--item">
+                  <p className="user-page__social--item--number">{userData.followersCount}</p>
+                  <p className="user-page__social--item--text">followers</p>
                 </div>
-              )}
+                <div className="user-page__social--item">
+                  <p className="user-page__social--item--number">{userData.followingCount}</p>
+                  <p className="user-page__social--item--text">following</p>
+                </div>
+                <div className="user-page__social--item">
+                  <p className="user-page__social--item--number">{myList.length ?? 0}</p>
+                  <p className="user-page__social--item--text">ítems in list</p>
+                </div>
+              </div>
+
+              <div className="user-page__header--banner-img--container">
+                <img className="banner-user--img" src={userData.avatar} alt={userData.avatar} />
+              </div>
             </div>
-        </div>
-        
-        {isOpenAddFavorite && (
+            <div className="user-page__header-user-info">
+              <h1>{userData.username}</h1>
+              <p>{userData.description}</p>
+            </div>
+          </header>
+
+          <div className="user-page__favorites-container">
+            <div className="user-page__favorites--header">
+              <h2>Tus favoritos</h2>
+              <button className="btn" onClick={() => setIsOpenRemoveFavorite(true)}>Edit favorites</button>
+            </div>
+              <div className="anime-cards__container">
+                {isLoadingFavorites ? (
+                  <LoadingComponent text="Charging favorite animes..."></LoadingComponent>
+                ) : isErrorFavorites ? (
+                  <ErrorComponent text="An error has ocurred"></ErrorComponent>
+                ) : (
+                  <>
+                    {myFavoriteList.map((anime: AnimeCardType) =>(
+                    <AnimeCard
+                      key={anime.id}
+                      anime={anime}
+                      variant="minimal"
+                    >
+                    </AnimeCard>
+                    ))}
+                    {myFavoriteList.length <= 4 && (
+                      <button className="favorite--add_btn" onClick={() => setIsOpenAddFavorite(true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--color-white" className="size-6 icon-size-xl">
+                          <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+                        </svg>
+                      </button>
+                    )}
+                  </>
+                )}
+              </div>
+          </div>
+
+          {isOpenAddFavorite && (
           <ModalAddFavorites
             listFavoriteId = {list}
             userId={user.uid}
@@ -88,10 +119,12 @@ const UserPage = () => {
             onClose={() => setIsOpenRemoveFavorite(false)}
           >
           </ModalRemoveFavorites>
-        )}      
+        )}    
+
+        
       </>
     )}
-    
+    </section>
   </>);
 };
 
