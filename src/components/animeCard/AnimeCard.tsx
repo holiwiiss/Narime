7dba07749b3eb4176af962772e2248eca647783b
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-05-24 11:14:02 +0200

    feature: directory responsive

--- src/components/animeCard/AnimeCard.tsx
+++ src/components/animeCard/AnimeCard.tsx
@@ -54,7 +54,7 @@ const AnimeCard = ({anime, userData, onOpenModal, variant="default"}: PropsAnime
                     <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
                   </svg>
                 )}
-                {userData ? "Edit" : "Add "}
+                <span className="btn-anime-card__text">{userData ? "Edit" : "Add"}</span>
               </button>
             )}
             
