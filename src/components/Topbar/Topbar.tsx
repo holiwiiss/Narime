8936a579f70d4fdc16beb6598e605ab39a344ed5
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-06-02 19:06:45 +0200

    fixed:errors

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -26,7 +26,7 @@ const Topbar = () => {
             <span className="text-p text-color--75 action-item__text">Home</span>
           </NavLink>
 
-           <NavLink to='/discover' className={({ isActive }) => `action-item nav--option ${isActive ? "nav--option--active" : ""}`}>
+          <NavLink to='/discover' className={({ isActive }) => `action-item nav--option ${isActive ? "nav--option--active" : ""}`}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="var(--color-white-50)" className="size-5 action-item__icon">
               <path fillRule="evenodd" d="M2.5 3A1.5 1.5 0 0 0 1 4.5v4A1.5 1.5 0 0 0 2.5 10h6A1.5 1.5 0 0 0 10 8.5v-4A1.5 1.5 0 0 0 8.5 3h-6Zm11 2A1.5 1.5 0 0 0 12 6.5v7a1.5 1.5 0 0 0 1.5 1.5h4a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 17.5 5h-4Zm-10 7A1.5 1.5 0 0 0 2 13.5v2A1.5 1.5 0 0 0 3.5 17h6a1.5 1.5 0 0 0 1.5-1.5v-2A1.5 1.5 0 0 0 9.5 12h-6Z" clipRule="evenodd" />
             </svg>
--- src/hooks/useMyListStats.ts
+++ src/hooks/useMyListStats.ts
@@ -4,20 +4,20 @@ export function useMyListStats() {
 
   const {myList} = useMyAnimeList()
   const mediumScore = () => {
-    const scoreList = myList.filter (anime => anime.scorePersonal)
-    const scoreSum = scoreList.reduce((total, anime) => total + (anime.scorePersonal ?? 0), 0)
+    const scoreList = myList.filter ((anime: any) => anime.scorePersonal)
+    const scoreSum = scoreList.reduce((total, anime):any => total + (anime.scorePersonal ?? 0), 0)
     return scoreSum/scoreList.length
   }
 
   const stats = {
     total: myList.length,
-    watching: myList.filter( anime => anime.statusPersonal === "watching").length,
-    completed: myList.filter( anime => anime.statusPersonal === "completed").length,
-    dropped: myList.filter( anime => anime.statusPersonal === "dropped").length,
-    planToWatch: myList.filter( anime => anime.statusPersonal === "planToWatch").length,
-    episodesWatched: myList.reduce((total, anime) => total + (anime.episodesWatched ?? 0), 0),
-    timeInHours: myList.reduce((total, anime) => total + (anime.episodesWatched ?? 0), 0) * 24 / 60,
-    timeInDays: myList.reduce((total, anime) => total + (anime.episodesWatched ?? 0), 0) * 24 / 60 / 24,
+    watching: myList.filter((anime: any) => anime.statusPersonal === "watching").length,
+    completed: myList.filter((anime: any) => anime.statusPersonal === "completed").length,
+    dropped: myList.filter((anime: any)=> anime.statusPersonal === "dropped").length,
+    planToWatch: myList.filter((anime: any) => anime.statusPersonal === "planToWatch").length,
+    episodesWatched: myList.reduce((total, anime):any => total + (anime.episodesWatched ?? 0), 0),
+    timeInHours: myList.reduce((total, anime):any => total + (anime.episodesWatched ?? 0), 0) * 24 / 60,
+    timeInDays: myList.reduce((total, anime):any => total + (anime.episodesWatched ?? 0), 0) * 24 / 60 / 24,
     scoreMedia: mediumScore() 
   }
 
