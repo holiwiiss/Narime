468bbeeb5a68e2c2dc737830eafd3e41ae286de7
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-06-07 17:24:44 +0200

    fixed: landing images

--- src/components/ui/landing/landing.tsx
+++ src/components/ui/landing/landing.tsx
@@ -1,7 +1,14 @@
-import IconHome from "../icons/icon-home"
+import directoryImg from "../../../assets/images/directory.png"
+import myListImg from "../../../assets/images/mylist.png"
+import profileImg from "../../../assets/images/profile.png"
 import "./landing.scss"
+import { useState } from "react"
+import IconUser from "../icons/icon-user"
 
 const Landing = () => {
+  const [activeBtn, setActiveBtn] = useState<string>("directory")
+  const [currentImage, setCurrentImage] = useState(directoryImg)
+  
   return(
     <>
       <header className="landing-header">
@@ -28,22 +35,28 @@ const Landing = () => {
           </header>
           <div className="landing-features__content">
             <div className="landing-features__content--item">
-              <IconHome className="icon-size-xl"/>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="var(--color-white)" className="size-6 icon-size-xl">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
               <h3 className="text-h3">Registra tu progreso</h3>
               <p>Marca episodios vistos, actualiza estados y puntúa cada anime mientras avanzas.</p>
             </div>
             <div className="landing-features__content--item">
-              <IconHome className="icon-size-xl"/>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="var(--color-white)" className="size-6 icon-size-xl">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
+              </svg>
               <h3 className="text-h3"> Tu lista, organizada</h3>
               <p>Gestiona fácilmente lo que estás viendo, completaste, abandonaste o planeas ver.</p>
             </div>
             <div className="landing-features__content--item">
-              <IconHome className="icon-size-xl"/>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="var(--color-white)" className="size-6 icon-size-xl">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+              </svg>
               <h3 className="text-h3">Descubre tu próximo anime</h3>
               <p>Explora títulos por temporada, popularidad, valoración y encuentra nuevas historias para añadir a tu lista.</p>
             </div>
             <div className="landing-features__content--item">
-              <IconHome className="icon-size-xl"/>
+              <IconUser fill="var(--color-white)" className="icon-size-xl"/>
               <h3 className="text-h3">Tu perfil de fan</h3>
               <p>Consulta estadísticas, favoritos y todo tu historial anime desde un único lugar.</p>
             </div>
@@ -57,20 +70,20 @@ const Landing = () => {
           </header>
           <div className="landing-screens__content">
             <ul className="landing-screens--options">
-              <li className="landing-screens--options--item">
+              <li className={`landing-screens--options--item ${activeBtn === "directory" ? "landing--item-active" : ""}`} onClick={() => {setActiveBtn("directory") ; setCurrentImage(directoryImg)}}>
                 <h3 className="text-h3">Directory</h3>
-                <p className="text-p text-color--75">Encuentra animes por género, temporada o popularidad y añade tus favoritos en segundos.</p>
+                <p className={`text-p text-color--75 ${activeBtn !== "directory" ? "visually-hidden" : ""}`}>Encuentra animes por género, temporada o popularidad y añade tus favoritos en segundos.</p>
               </li>
-              <li className="landing-screens--options--item">
-                <h3 className="text-h3">Search</h3>
-                <p className="text-p text-color--75 visually-hidden">Encuentra animes por género, temporada o popularidad y añade tus favoritos en segundos.</p>
+              <li className={`landing-screens--options--item ${activeBtn === "mylist" ? "landing--item-active" : ""}`} onClick={() => {setActiveBtn("mylist"); setCurrentImage(myListImg)}}>
+                <h3 className="text-h3">My List</h3>
+                <p className={`text-p text-color--75 ${activeBtn !== "mylist" ? "visually-hidden" : ""}`}>Organiza tu colección personal y mantén siempre actualizado tu progreso.</p>
               </li>
-              <li className="landing-screens--options--item">
+              <li className={`landing-screens--options--item ${activeBtn === "profile" ? "landing--item-active" : ""}`} onClick={() => {setActiveBtn("profile"); setCurrentImage(profileImg)}}>
                 <h3 className="text-h3">Profile</h3>
-                <p className="text-p text-color--75 visually-hidden">Encuentra animes por género, temporada o popularidad y añade tus favoritos en segundos.</p>
+                <p className={`text-p text-color--75 ${activeBtn !== "profile" ? "visually-hidden" : ""}`}>Visualiza tus estadísticas, puntuaciones y series favoritas de un vistazo.</p>
               </li>
             </ul>
-            <img src="#" className="landing-screens__image" />
+            <img src={currentImage} className="landing-screens__image"/>
           </div>
         </section>
       </main>
