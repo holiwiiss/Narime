435afd370dd6d586568c50893cdd884b0269a656
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-06-15 13:12:30 +0200

    fixed: errors responsive

--- src/view/anime-information/anime-information.tsx
+++ src/view/anime-information/anime-information.tsx
@@ -328,23 +328,23 @@ const AnimePage = () => {
             
         </section>
       </div>
-        <button className="btn anime-page__btn action-item" 
+        <button className={`btn action-item anime-page__btn ${userData ? "btn--secondary" : ""}`}
             aria-label={userData ? "Edit anime in list" : "Add anime to my list"}
             onClick={()  => openAddEditModal(animeInfo)}
           >
             {userData ? (
               <>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--color-white)" className="size-6 icon-size-m" aria-hidden="true">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--color-white)" className="size-6 action-item__icon" aria-hidden="true">
                 <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
               </svg>
-              <span className="text-btn">Edit Anime</span>
+              <span className="text-btn action-item__text anime-page__text-btn">Edit Anime</span>
               </>
             ):(
               <>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--color-white)" className="size-6 icon-size-m" aria-hidden="true">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--color-white)" className="size-6 action-item__icon" aria-hidden="true">
                   <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
                 </svg>
-                <span className="text-btn">Add to your list</span>
+                <span className="text-btn action-item__text anime-page__text-btn">Add to your list</span>
               </>
             )}
           </button>
