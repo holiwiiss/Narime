8a03ba2f6be0303b57397261992ea1e5ddc88e77
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-06-15 14:19:19 +0200

    fixed: errors

--- src/components/main-topbar/main-topbar.tsx
+++ src/components/main-topbar/main-topbar.tsx
@@ -28,7 +28,7 @@ const Topbar = () => {
 
         <nav className="topbar--nav">
           <NavLink to='/directory' className={({ isActive }) => `action-item nav--option ${isActive ? "nav--option--active" : ""}`}>
-            <IconHome className="size-6 action-item__icon"/>
+            <IconMylist className="size-6 action-item__icon"/>
             <span className="text-p text-color--75 action-item__text">Explore</span>
           </NavLink>
 
@@ -39,7 +39,9 @@ const Topbar = () => {
 
           {user && 
           <NavLink to='/my-list' className={({ isActive }) => `action-item nav--option ${isActive ? "nav--option--active" : ""}`}>
-            <IconMylist className="size-6 action-item__icon"/>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--color-white-50)" className="size-6 action-item__icon">
+              <path fillRule="evenodd" d="M10 2c-1.716 0-3.408.106-5.07.31C3.806 2.45 3 3.414 3 4.517V17.25a.75.75 0 0 0 1.075.676L10 15.082l5.925 2.844A.75.75 0 0 0 17 17.25V4.517c0-1.103-.806-2.068-1.93-2.207A41.403 41.403 0 0 0 10 2Z" clipRule="evenodd" />
+            </svg>
             <span className="text-p text-color--75 action-item__text">My List</span>
           </NavLink>}
 
